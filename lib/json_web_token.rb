19f98d4069826b0221ae53bec001d8683abd1edd
Kevin Louis <keviinlouiis@gmail.com>
2020-07-06 15:08:03 -0300

    added env SECRET_KEY_BASE on jwt

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,12 +2,12 @@ class JsonWebToken
   class << self
     def encode(payload, expiry = 24.hours.from_now)
       payload[:expiry] = expiry.to_i
-      secret_key_base = Rails.application.secrets.secret_key_base
+      secret_key_base = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
       JWT.encode(payload, secret_key_base, 'HS256')
     end
 
     def decode(token)
-      secret_key_base = Rails.application.secrets.secret_key_base
+      secret_key_base = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base
       body = JWT.decode(token, secret_key_base, 'HS256')[0]
       HashWithIndifferentAccess.new body
     rescue
